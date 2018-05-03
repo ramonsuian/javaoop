fc00883e81d1ba33ca053b7192d79468c921281e
ramonsuian <35726788@qq.com>
2018-05-03 11:22:53 +0800

    作业

---
+++ 作业/src/作业/Ramon.java
@@ -0,0 +1,16 @@
+package 作业;
+
+import java.util.Scanner;
+
+public class Ramon {
+   public static void main(String[] args) {
+   System.out.println("请输入一个年份");
+   Scanner scanner = new Scanner(System.in);
+   int year = scanner.nextInt();
+   if (year%400 == 0 ||( year%4== 0 && year%100!= 0) ) {
+	   System.out.println("闰年");
+   }else {
+	   System.out.println("平年");
+   }
+}
+}
